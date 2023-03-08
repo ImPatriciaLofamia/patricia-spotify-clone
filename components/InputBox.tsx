7eb6bb3c3ab3eb9dbcee0ba946c491d1dc4cd304
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-03-08 22:37:38 +0800

    REFACTOR: Add condition in input component

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -21,7 +21,7 @@ const InputBox = ({
     {label && <p className="text-sm font-bold"> {label} </p>}
     <input
       className={`${className} bg-white border pb-3 border-gray-400 text-base text-black font-medium rounded-lg
-    ${type && "accent-green-500"} `}
+    ${type="checkbox" && "accent-green-500"} `}
       placeholder={placeholder}
       type={type}
       value={value}
