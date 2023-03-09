4ce75743283977de18b3e6cc06f92153fda31135
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-03-09 09:23:03 +0800

    REFACTOR: Custom components button, input, navi

--- components/SideBarNav.tsx
+++ components/SideBarNav.tsx
@@ -1,43 +1,54 @@
 import React from "react";
-import { RssIcon ,HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon } from "@heroicons/react/outline";
+import {
+  RssIcon,
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  HeartIcon,
+} from "@heroicons/react/outline";
+import Button from "./Button";
 
 const SideBarNav = () => {
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-3 text-sm border-r border-gray-900">
       <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="w-5 h-5 " />
-          <p>Home</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <SearchIcon className="w-5 h-5 " />
-          <p>Search</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="w-5 h-5 " />
-          <p>My Library</p>
-        </button>
+        <Button
+          className="flex items-center space-x-2 justify-start hover:text-white border-0"
+          buttonName="Home"
+          icon={<HomeIcon className="w-5 h-5" />}
+        />
+        <Button
+          className="flex items-center space-x-2 justify-start hover:text-white border-0"
+          buttonName="Search"
+          icon={<SearchIcon className="w-5 h-5" />}
+        />
+        <Button
+          className="flex items-center space-x-2 justify-start hover:text-white border-0"
+          buttonName="My Library"
+          icon={<LibraryIcon className="w-5 h-5" />}
+        />
         <hr className="border-t-[0.1px] border-gray-900"></hr>
-
-        <button className="flex items-center space-x-2 hover:text-white">
-          <PlusCircleIcon className="w-5 h-5 " />
-          <p>Create Playlist</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="w-5 h-5 " />
-          <p>Liked Song</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="w-5 h-5 " />
-          <p>Episode</p>
-        </button>
+        <Button
+          className="flex items-center space-x-2 justify-start hover:text-white border-0"
+          buttonName="Create Playlist"
+          icon={<PlusCircleIcon className="w-5 h-5" />}
+        />
+        <Button
+          className="flex items-center space-x-2 justify-start hover:text-white border-0"
+          buttonName="Liked Song"
+          icon={<HeartIcon className="w-5 h-5" />}
+        />
+        <Button
+          className="flex items-center space-x-2 justify-start hover:text-white border-0"
+          buttonName="Episodes"
+          icon={<RssIcon className="w-5 h-5" />}
+        />
         <hr className="border-t-[0.1px] border-gray-900"></hr>
-
-        {/* This is where playlist would be rendered */}
         <p className=" cursor-pointer hover:text-white">Playlist name...</p>
       </div>
     </div>
   );
-}
+};
 
 export default SideBarNav;
