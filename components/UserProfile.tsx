cc0e1ce03813f1aee3100c6a56d1cb9de9522538
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-03-11 20:37:29 +0800

    refactor: change to camel case

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,14 +1,14 @@
 interface UserProps {
     image: any,
-    username: any,
+    userName: any,
     className: string,
     dropdown: any
 }
-const UserProfile = ({image, username, className, dropdown}: UserProps) => {
+const UserProfile = ({image, userName, className, dropdown}: UserProps) => {
     return(
         <div className="flex items-center">
             <img src={image} className={`${className}sm: w-10 h-10`}/>
-            <p className="text-base hidden sm:block">{username}</p>
+            <p className="text-base hidden sm:block">{userName}</p>
             <button
                 onClick={() => alert('Menu: Account, Profile, Get Premium, Settings, Logout')}
                 className=""
